14efc370ce818a76e4bd3650f99938c83f025666
Coderovshik <peter.04007@gmail.com>
2023-03-25 17:13:02 +0300

    cringe

--- src/Grid.js
+++ src/Grid.js
@@ -2,9 +2,9 @@ import { useState } from 'react'
 
 function generateEmptyGrid(width, height, gridCell) {
     let cells = [];
-    for (let i = 0; i < h; i++) {
+    for (let i = 0; i < height; i++) {
         let temp = [];
-        for (let j = 0; j < w; j++) {
+        for (let j = 0; j < width; j++) {
             temp.push(gridCell);
         }
         cells.push(temp);
